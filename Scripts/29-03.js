1976bb4b5a63f8e1566da0da55803191db1a29e0
Francisco Javier Arrarte <30293668+arrartefrancisco@users.noreply.github.com>
2021-03-29 15:02:20 -0300

    Add files via upload

---
+++ Scripts/29-03.js
@@ -0,0 +1,16 @@
+let teamName = "formacion";
+let people = ["Amalia", "Karla","Silvana", "Manu", "Jonh", "Mike"];
+
+(teamName, people) => window.alert(`Hay ${people.length} en el equipo de ${teamName}.\nEllos son ${people.toString()}`);
+
+//
+// Para testing
+//
+/*
+function Teams(teamName, people)
+{
+    window.alert(`Hay ${people.length} en el equipo de ${teamName}.\nEllos son ${people.toString()}`);
+}
+*/
+
+
